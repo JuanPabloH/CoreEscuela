2b143d79ac3cd2069627f4c073fd4a97ebcd3e0e
Juan Pablo Herrera <juan.herrera02@uptc.edu.co>
2019-08-23 17:09:04 -0500

    inicio

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public string ceo;
+        public int anhoFundacion;
+
+        public void timbrar()
+        {
+            Console.Beep(2000, 3000);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            var miEscuela= new Escuela();
+            miEscuela.nombre= "Nueva Academia";
+            miEscuela.direccion= "Carrera 12 ";
+            miEscuela.ceo="Juan Herrera";
+            miEscuela.anhoFundacion= 2001;
+            Console.WriteLine("Timbre");
+            miEscuela.timbrar();
+        }
+    }
+}
