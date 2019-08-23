1dce2b795f7463fc3bfda5858d808630185ad652
Juan Pablo Herrera <juan.herrera02@uptc.edu.co>
2019-08-23 17:49:02 -0500

    Creación estructura basica de la escuela

---
+++ Etapa1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CoreEscuela.Entidades;
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var escuela=new Escuela("Mi escuelita",2010);
+            escuela.Pais= "Colombia";
+            escuela.Ciudad= "Sogamoso";
+            Console.WriteLine(escuela.Nombre);
+        }
+    }
+}
