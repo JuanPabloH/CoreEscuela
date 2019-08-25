634022203a73201ddd93770406a9a4ea6c602e66
Juan Pablo Herrera <juan.herrera02@uptc.edu.co>
2019-08-24 22:43:00 -0500

    sobrecarga de metodos y listas de solo lectura

--- Etapa5/App/EscuelaEngine.cs
+++ Etapa5/App/EscuelaEngine.cs
@@ -25,6 +25,123 @@ namespace CoreEscuela
 
         }
 
+
+        
+        public IReadOnlyList<ObjetoEscuelaBase> GetObjetosEscuela(
+            bool traeEvalicaciones= true,
+            bool traeAlumnos= true,
+            bool traeAsignaturas= true,
+            bool traeCursos= true
+            
+            )
+            {
+                return GetObjetosEscuela(out int dummy,out dummy,out dummy,out dummy);
+            }
+
+
+        public IReadOnlyList<ObjetoEscuelaBase> GetObjetosEscuela(
+            out int contEvaluaciones,
+            bool traeEvalicaciones= true,
+            bool traeAlumnos= true,
+            bool traeAsignaturas= true,
+            bool traeCursos= true
+            
+            )
+         {
+             return GetObjetosEscuela(out contEvaluaciones,out int dummy,out dummy,out dummy);
+         }
+
+
+        public IReadOnlyList<ObjetoEscuelaBase> GetObjetosEscuela(
+            out int contEvaluaciones,
+            out int contCursos,
+            bool traeEvalicaciones= true,
+            bool traeAlumnos= true,
+            bool traeAsignaturas= true,
+            bool traeCursos= true
+            
+            )
+        {
+            return GetObjetosEscuela(out contEvaluaciones,out contCursos,out int dummy,out dummy);
+        }
+
+
+        public IReadOnlyList<ObjetoEscuelaBase> GetObjetosEscuela(
+            out int contEvaluaciones,
+            out int contCursos,
+            out int contAsignaturas,
+            bool traeEvalicaciones= true,
+            bool traeAlumnos= true,
+            bool traeAsignaturas= true,
+            bool traeCursos= true
+            
+            )
+        {
+            return GetObjetosEscuela(out contEvaluaciones,out contCursos,out contAsignaturas,out int dummy);
+        }
+        public IReadOnlyList<ObjetoEscuelaBase> GetObjetosEscuela(
+            out int contEvaluaciones,
+            out int contCursos,
+            out int contAsignaturas,
+            out int contAlumnos,
+            bool traeEvalicaciones= true,
+            bool traeAlumnos= true,
+            bool traeAsignaturas= true,
+            bool traeCursos= true
+            
+            )
+        {
+            contAsignaturas=contAlumnos=contEvaluaciones=0;
+            
+            var listaObj= new List<ObjetoEscuelaBase>();
+            listaObj.Add(Escuela);
+            if (traeCursos)
+            {
+                listaObj.AddRange(Escuela.Cursos);
+            }
+            contCursos=Escuela.Cursos.Count;
+            foreach (var curso in Escuela.Cursos)
+            {   
+                contAsignaturas += curso.Asignatura.Count;
+                contAlumnos+= curso.Alumno.Count;
+                if (traeAsignaturas)
+                {
+                    listaObj.AddRange(curso.Asignatura);   
+                }                
+                if (traeAlumnos)
+                {
+                    listaObj.AddRange(curso.Alumno);
+                }
+                
+                if (traeEvalicaciones)
+                {
+                    foreach (var alumno in curso.Alumno)
+                    {
+                        listaObj.AddRange(alumno.Evaluaciones);
+                        contEvaluaciones+=alumno.Evaluaciones.Count;
+                    }
+                }
+                
+            }
+            
+            return listaObj.AsReadOnly();
+        }
+
+
+#region Métodos de carga
+        private List<Alumno> GenerarAlumnos(int cantidad)
+        {
+            string [] nombre1={"Alba","Juan","Viviana","Slendy","Laura","Crisito","Pablo"};
+            string [] nombre2={"Pedra","Pabla","Jose","Felipe","Andres","Manuel","Eusebio"};
+            string [] apellido={"Herrera","Rodriguez","Stark","Rogers","Carter","Petro","Jeje"};
+
+            var listaAlumnos =  from n1 in nombre1
+                                from n2 in nombre2
+                                from ap in apellido
+                                select new Alumno{Nombre=$"{n1} {n2} {ap}"};
+            return listaAlumnos.OrderBy((alum)=> alum.UniqueId).Take(cantidad).ToList();
+            
+        }
         private void CargarEvaluaciones()
         {
             foreach (var curso in Escuela.Cursos)
@@ -66,19 +183,7 @@ namespace CoreEscuela
             }
         }
 
-        private List<Alumno> GenerarAlumnos(int cantidad)
-        {
-            string [] nombre1={"Alba","Juan","Viviana","Slendy","Laura","Crisito","Pablo"};
-            string [] nombre2={"Pedra","Pabla","Jose","Felipe","Andres","Manuel","Eusebio"};
-            string [] apellido={"Herrera","Rodriguez","Stark","Rogers","Carter","Petro","Jeje"};
 
-            var listaAlumnos =  from n1 in nombre1
-                                from n2 in nombre2
-                                from ap in apellido
-                                select new Alumno{Nombre=$"{n1} {n2} {ap}"};
-            return listaAlumnos.OrderBy((alum)=> alum.UniqueId).Take(cantidad).ToList();
-            
-        }
 
         private void CargarCursos()
         {
@@ -98,24 +203,9 @@ namespace CoreEscuela
             }
         }
 
-        public List<ObjetoEscuelaBase> GetObjetosEscuela()
-        {
-            var listaObj= new List<ObjetoEscuelaBase>();
-            listaObj.Add(Escuela);
-            listaObj.AddRange(Escuela.Cursos);
-            
-            foreach (var curso in Escuela.Cursos)
-            {
-                listaObj.AddRange(curso.Asignatura);
-                listaObj.AddRange(curso.Alumno);
-                foreach (var alumno in curso.Alumno)
-                {
-                    listaObj.AddRange(alumno.Evaluaciones);
-                }
-            }
-            
-            return listaObj;
-        }
+       #endregion Métodos de carga 
+    
+    
     }
 
 }
